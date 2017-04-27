8978fd38407e9aeeae092ed16d933957dd830546
primeMover2357 <cmcdon22@gmail.com>
2017-04-27 17:01:33 -0500

    Added the original 3 files.
    I selected some relevant files from a larger python directory on my computer.

---
+++ scraper.py
@@ -0,0 +1,32 @@
+#Test Url https://www.reddit.com/r/aww/comments/5vj9ut/these_cows_look_like_theyre_about_to_drop_the/
+
+import praw
+from desinc import comment_karma
+
+
+reddit = praw.Reddit(user_agent="Descending Increase",
+                     client_id="d5n8CMZMGx52yQ",
+                     client_secret="l54jhktdoxXFWNfOVM8FY2AnolI")
+
+page_url = raw_input('URL ')
+
+submission = reddit.submission(url = page_url)
+submission.comments.replace_more(limit=0)
+
+#top_level_comments = list(submission.comments)
+all_comments = submission.comments.list()
+
+for comment in all_comments:
+    if comment.is_root == False:
+        parent = comment.parent()
+        if comment_karma(comment) > comment_karma(parent):
+          percent_change = (comment_karma(comment) - comment_karma(parent)) / comment_karma(parent)           
+          print comment_karma(parent), '----->', comment_karma(comment), '||||||||',  parent.fullname, '----->' , comment.fullname, 'RATIO: ', percent_change
+          print(parent.body)
+          print 90 * '-'
+          print(comment.body)
+          print 90 * '*'
+          print 90 * '*'
+
+print('Finished searching')
+
