be2855bc82b5240ad911e9b48df43274788112db
cmcdon22 <cmcdon22@outlook.com>
2025-06-25 17:38:23 -0500

    Hide secrets

--- desinc.py
+++ desinc.py
@@ -1,8 +1,17 @@
 import praw
+from dotenv import load_dotenv
+import os
 
-reddit = praw.Reddit(user_agent="Descending Increase",
-					 client_id="d5n8CMZMGx52yQ",
-					 client_secret="l54jhktdoxXFWNfOVM8FY2AnolI")
+
+load_dotenv()  # Load variables from .env into environment
+
+
+reddit = praw.Reddit(
+    client_id=os.getenv("REDDIT_CLIENT_ID"),
+    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+    user_agent=os.getenv("REDDIT_USER_AGENT")
+)
+
 
 def rfunction(submission_id):
 
@@ -19,7 +28,8 @@ def rfunction(submission_id):
             print('\nIn ratio ', karma, '-->', comment_karma(comment), '/')
     else:
         print('Nothing to see here')
-		
+
+
 
 def comment_karma(comment):
     return comment.ups - comment.downs
